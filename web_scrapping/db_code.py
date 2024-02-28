f40586bcafaacdc983df43e4aca35fd3f643d995
alditus <alvarez.aldoandres@gmail.com>
2024-02-27 21:53:15 -0300

    Added code to append data to a sql table

--- web_scrapping/db_code.py
+++ web_scrapping/db_code.py
@@ -12,3 +12,32 @@ df = pd.read_csv(file_path, names = attribute_list)
 df.to_sql(table_name, conn, if_exists = 'replace', index =False)
 print('Table is ready')
 
+# TRYING DIFFERENTE QUERIES TO TEST OUR KNOWLEDGE OF SQL QUERIES
+query_statement = f"SELECT * FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
+
+# GET ONLY ONE COLUMN
+query_statement = f"SELECT FNAME FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
+
+# GET THE NUMBER OF RECORDS IN THE TABLE
+query_statement = f"SELECT COUNT(*) FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
+
+data_dict = {'ID' : [100],
+            'FNAME' : ['John'],
+            'LNAME' : ['Doe'],
+            'CITY' : ['Paris'],
+            'CCODE' : ['FR']}
+data_append = pd.DataFrame(data_dict)
+
+data_append.to_sql(table_name, conn, if_exists = 'append', index =False)
+print('Data appended successfully')
+conn.close()
+
